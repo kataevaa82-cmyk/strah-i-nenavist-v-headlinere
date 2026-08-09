80b65f5eba632ffcc535e155165c4f4a9cd87a7d
kataevaa82-cmyk <kataevaa82-cmyk@users.noreply.github.com>
2026-08-09 21:35:18 +0700

    Добавить прицел в браузерную версию для ПК

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786285529|10673661';
+const CACHE_VERSION = '1786286044|134341844';
 /** @type {string} */
 const CACHE_PREFIX = 'Страх и ненавист-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
