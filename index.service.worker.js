6b758864f79c4df1956d6661ce8f4a6337538144
kataevaa82-cmyk <kataevaa82-cmyk@users.noreply.github.com>
2026-08-09 22:29:29 +0700

    Упростить управление и добавить точный бросок

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786286044|134341844';
+const CACHE_VERSION = '1786289275|137270250';
 /** @type {string} */
 const CACHE_PREFIX = 'Страх и ненавист-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
