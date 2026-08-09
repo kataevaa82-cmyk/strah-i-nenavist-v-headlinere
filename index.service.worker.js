ae66ce192f6a1ec4aa993cb23e74dc229f56a6c1
kataevaa82-cmyk <kataevaa82-cmyk@users.noreply.github.com>
2026-08-09 21:25:47 +0700

    Добавить мобильный прицел на комика

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786284912|12892158';
+const CACHE_VERSION = '1786285529|10673661';
 /** @type {string} */
 const CACHE_PREFIX = 'Страх и ненавист-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
